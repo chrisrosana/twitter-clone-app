f3afcc3c8f4b17432b93be0df29797873f6788b3
chrisrosana <chrisrosana@yahoo.com>
2019-03-02 21:14:17 -0800

    add feature character count

---
+++ Twitter/TweetViewController.swift
@@ -0,0 +1,72 @@
+//
+//  TweetViewController.swift
+//  Twitter
+//
+//  Created by Christopher Rosana on 2/25/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetViewController: UIViewController, UITextViewDelegate {
+
+    @IBOutlet weak var characterCount: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tweetTextView.becomeFirstResponder()
+        
+        tweetTextView.delegate = self
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var tweetTextView: UITextView!
+    
+    @IBAction func cancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func tweet(_ sender: Any) {
+        
+        if(!tweetTextView.text.isEmpty) {
+            TwitterAPICaller.client?.postTweet(tweetString: tweetTextView.text, success: {
+                self.dismiss(animated: true, completion: nil)
+            }, failure: { (error) in
+                print("Error posting tweet \(error)")
+                self.dismiss(animated: true, completion: nil)
+            })
+        } else {
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        
+        // Set the max character limit
+        let characterLimit = 140
+        
+        // Construct what the new text would be if we allowed the user's latest edit
+        let newText = NSString(string: tweetTextView.text!).replacingCharacters(in: range, with: text)
+        
+        // TODO: Update Character Count Label
+        characterCount.text = String(characterLimit - newText.characters.count)
+        
+        // The new text should be allowed? True/False
+        return newText.characters.count < characterLimit
+        
+        // TODO: Check the proposed new text character count
+        // Allow or disallow the new text
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
